942fd78ebd207ad484f7ec50297c778c7df856d2
Sagoogoolmoongy <70049299+Sagoogoolmoongy@users.noreply.github.com>
2021-03-22 08:53:40 +0900

    Add files via upload

---
+++ Discord2.py
@@ -0,0 +1,22 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("디스코드 봇 실행 성공!")
+    game = discord.Game("사구굴뭉이 봇")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!안녕"):
+        await message.channel.send("안녕하세요 사구굴뭉이 봇입니다")
+
+
+client.run("ODIzMDI2Njc4OTQ2NDYzNzU0.YFa1Uw.NvYlKfsVD_RWJW9GR54iID3ESnA")
+
